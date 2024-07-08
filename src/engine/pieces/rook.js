3cd0ba49ea35e2d94168b2e0516825e26ebd2983
Ruksan Emirali <ruksan.emirali@softwire.com>
2024-07-08 15:33:11 +0100

    red-3 (rook) rests now passing

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +7,25 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let arr =[];
+        // loop through all horizontal and vertical squares and store position (except current square) to array
+        for (let i=0;i < 8; i++)
+        {
+            if (location.row !=i ){
+                arr.push({"row": i , "col" : location.col})
+            }
+            if (location.col !=i ){
+                arr.push({"row": location.row , "col" : i})
+            
+        }
+        return arr;
     }
 }
+
+// find current position
+// check if vertical up move is available - loop for consecutive squares
+// check if vertical down move is available
+// check if horizontal left move is available 
+// check if horizontal right move is available 
+// push to each possible available move to an array
