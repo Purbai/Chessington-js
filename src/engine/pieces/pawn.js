867a49962606de16f333e8e3854bf467594c659d
Ruksan Emirali <ruksan.emirali@softwire.com>
2024-07-08 14:18:04 +0100

    pawns test cases passed

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -10,9 +10,18 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
+            if (this.hasPieceMoved) {
+                return [Square.at(location.row + 1, location.col)]
+            } else {
+                return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+            }
+
         } else {
-            return Square.at(location.row - 1, location.col)
+            if (this.hasPieceMoved) {
+                return [Square.at(location.row - 1, location.col)]
+            } else {
+                return [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
+            }
         }
     }
 }
