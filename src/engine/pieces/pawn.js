353a0d8002676c4b27cb89cf904fa0eb627b4ef9
Purbai Varsani <purbai.varsani@softwire.com>
2024-07-09 09:46:30 +0100

    added checks for cannot move if one or two squares in front are occupied

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -8,19 +8,69 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        let location = board.findPiece(this)
+        let location = board.findPiece(this);
+        let checkGetPiece = [];
+        let checkGetPiece2 = [];
         if (this.player === Player.WHITE) {
             if (this.hasPieceMoved) {
-                return [Square.at(location.row + 1, location.col)]
+                // check if the one square up has a piece on it
+                checkGetPiece = board.getPiece(Square.at(location.row + 1, location.col)); // return the piece that is on this square
+                if (checkGetPiece === undefined)  //no piece found on the one square up hence available
+                {
+                    return [Square.at(location.row + 1, location.col)]
+                }
+                else
+                {
+                    return [] //square was occupied hence pass empty 
+                }
             } else {
-                return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+                // check if one or two square up have pieces on it
+                checkGetPiece = board.getPiece(Square.at(location.row + 1, location.col)); // return the piece that is on this square
+                checkGetPiece2 = board.getPiece(Square.at(location.row + 2, location.col)); // return the piece that is on this square
+                if (checkGetPiece === undefined && checkGetPiece2 === undefined) // neither squares have pieces on them hence available
+                {
+                    return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+                }
+                else if (checkGetPiece === undefined) // one square up does not have a piece that is on it hence available
+                    {
+                        return [Square.at(location.row + 1, location.col)]
+                    }
+                else
+                {
+                    return [] //squares were occupied hence pass empty 
+                }
             }
 
         } else {
             if (this.hasPieceMoved) {
-                return [Square.at(location.row - 1, location.col)]
+                // check if the one square down has a piece on it
+                checkGetPiece = board.getPiece(Square.at(location.row - 1, location.col)); // return the piece that is on this square
+                if (checkGetPiece === undefined)  //no piece found on the one square down hence available
+                {
+                    return [Square.at(location.row - 1, location.col)]
+                }
+                else
+                {
+                    return [] //square was occupied hence pass empty 
+                }
             } else {
-                return [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
+
+                // check if one or two square down have pieces on it
+                checkGetPiece = board.getPiece(Square.at(location.row - 1, location.col)); // return the piece that is on this square
+                checkGetPiece2 = board.getPiece(Square.at(location.row - 2, location.col)); // return the piece that is on this square
+                if (checkGetPiece === undefined && checkGetPiece2 === undefined) // neither squares have pieces on them hence available
+                {
+                    return [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
+                }
+                else if (checkGetPiece === undefined) // one square down does not have a piece that is on it hence available
+                        {
+                            return [Square.at(location.row - 1, location.col)]
+                        }
+                else
+                {
+                    return [] //squares were occupied hence pass empty 
+                }
+  
             }
         }
     }
