41ced42bd4b142eea837e0ba2faa374cc7400cf0
Purbai <purbai.varsani@softwire.com>
2024-07-10 11:50:14 +0100

    Passes Red-9 tests

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,5 +1,7 @@
 import Piece from './piece';
 import Square from '../square';
+import Player from '../player';
+
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -9,15 +11,44 @@ export default class Rook extends Piece {
     getAvailableMoves(board) {
         let location = board.findPiece(this);
         let arr =[];
+        let checkGetPiece
         // loop through all horizontal and vertical squares and store position (except current square) to array
         for (let i=0;i < 8; i++)
-        {
+        {   
             if (location.row !=i ){
-                arr.push({"row": i , "col" : location.col})
+                // check piece
+                checkGetPiece = board.getPiece(Square.at(i, location.col)) 
+                // square is unoccupied
+                if( checkGetPiece === undefined ) {
+                    arr.push({"row": i , "col" : location.col}) // push to available squares
+                } 
+                // check if square is occupied by opposing piece
+                else if (checkGetPiece !== undefined && checkGetPiece.player !== this.player) {
+                    arr.push({"row": i , "col" : location.col})
+                    break
+                } 
+                // if path is blocked, break out of loop
+                else {
+                    break
+                }
             }
+
             if (location.col !=i ){
-                arr.push({"row": location.row , "col" : i})
-            
+                // check piece
+                checkGetPiece = board.getPiece(Square.at(location.row, i)) 
+                // check if square is unoccupied
+                if( checkGetPiece === undefined ) {
+                    arr.push({"row": location.row , "col" : i})
+                } 
+                // check if square is unoccupied by opposing piece
+                else if (checkGetPiece !== undefined && checkGetPiece.player !== this.player) {
+                    arr.push({"row": location.row , "col" : i})
+                    break
+                } 
+                // if path is blocked, break out of loop
+                else {
+                    break
+                }
         }
     }
         return arr;
